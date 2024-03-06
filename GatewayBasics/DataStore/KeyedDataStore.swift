4fb70e0647d2114c1b48dc4c84481a84b7ca2a27
Porter McGary <56183563+mcgaryp@users.noreply.github.com>
2024-03-06 15:12:52 -0700

    something

--- GatewayBasics/DataStore/KeyedDataStore.swift
+++ GatewayBasics/DataStore/KeyedDataStore.swift
@@ -9,14 +9,16 @@ import Foundation
 import UseCaseBasics
 
 /// A protocol for a keyed data store, where elements are uniquely identified by their ID.
-public protocol KeyedDataStore<Element>: DataStore where Payload == [Element.ID: Element] {
+public protocol KeyedDataStore<Key, Element>: DataStore where Payload == [Key: Element] {
+    associatedtype Key: Hashable
+    
     /// The type of elements stored in the data store, conforming to Identifiable.
-    associatedtype Element: Identifiable
+    associatedtype Element
     
     /// Retrieves an element from the data store by its ID.
     /// - Parameter id: The ID of the element to retrieve.
     /// - Returns: A result containing the element on success, or an error on failure.
-    func get(by id: Element.ID) -> DataResult<Element>
+    func get(by id: Key) -> DataResult<Element>
     
     /// Sets or updates an element in the data store.
     /// - Parameter element: The element to be set or updated.
@@ -24,13 +26,13 @@ public protocol KeyedDataStore<Element>: DataStore where Payload == [Element.ID:
     
     /// Clears an element from the data store by its ID.
     /// - Parameter id: The ID of the element to clear.
-    func clear(by id: Element.ID)
+    func clear(by id: Key)
     
     /// Clears an element from the data store.
     /// - Parameter id: The ID of the element to refresh.
-    func refresh(by id: Element.ID) async -> DataResult<Element>
+    func refresh(by id: Key) async -> DataResult<Element>
     
     /// Clears an element from the data store.
     /// - Parameter id: The ID of the element to refresh.
-    func refresh(by id: Element.ID)
+    func refresh(by id: Key)
 }
